e2d6445fb3204d266ad8803e9773938bbc1ab618
Monica <154905242+verhbal@users.noreply.github.com>
2025-05-28 00:37:49 +0700

    Remove Theme Song

--- frontend-hapi/src/App.jsx
+++ frontend-hapi/src/App.jsx
@@ -1,18 +1,10 @@
-// src/App.jsx
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/Routes';
-import BackgroundMusic from './components/BackgroundMusic'; // Import komponen musik Anda
 
 function App() {
   return (
     <BrowserRouter>
-      {/* Komponen musik diletakkan di sini. */}
-      {/* Ini akan memastikan BackgroundMusic selalu ada dan aktif */}
-      {/* di seluruh aplikasi, tidak peduli rute mana yang sedang aktif. */}
-      <BackgroundMusic />
-      
-      {/* AppRoutes akan menangani semua rute dan tampilan spesifik */}
       <AppRoutes />
     </BrowserRouter>
   );
